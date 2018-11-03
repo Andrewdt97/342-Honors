4592f0b6ca6f504a3466d119c2ed716a8847d82a
Tyler Vredeveld <tjv37@calvin.edu>
2018-11-03 11:52:41 -0400

    Edits of procedures

--- DemandProc.sql
+++ DemandProc.sql
@@ -9,35 +9,37 @@ as
 	
 	if object_id('DemandCalc') is null
 		create table DemandCalc(
-			Name varchar(128),
+			ID int,
 			Quantity int)
 
 	insert into DemandCalc 
 
-		select [Name], sum([Quantity]) from (
+		select [ProductID], sum([Quantity]) from (
 
-		select p.Name [Name], sum(sd.OrderQty) [Quantity] from Production.Product p
+		select p.ProductID [ProductID], sum(sd.OrderQty) [Quantity] from Production.Product p
 		inner join Sales.SalesOrderDetail sd on sd.ProductID = p.ProductID
 		inner join Sales.SalesOrderHeader sh on sd.SalesOrderID = sh.SalesOrderID
 		-- could be used in case the Status column isn't always reliable and we want to check by date instead
 		--where (sh.OrderDate <= convert(datetime, '2008-07-01') and convert(datetime, '2008-07-01') <= sh.DueDate)
 		where sh.Status = 1 or sh.Status = 2 or sh.Status = 3
-		group by p.Name
+		group by p.ProductID
 
 	union
 
-		SELECT pro.Name, SUM(sod.OrderQty) FROM 
+		SELECT pro.ProductID, SUM(sod.OrderQty) FROM 
 		Production.Product pro JOIN Sales.SalesOrderDetail sod
 			ON pro.ProductID = sod.ProductID
 		JOIN Sales.SalesOrderHeader soh ON sod.SalesOrderID = soh.SalesOrderID
 		WHERE DATEPART(DAYOFYEAR, OrderDate) > DATEPART(DAYOFYEAR, Convert(datetime, '2006-01-01' )) -- Will become GETDATE - numOfDays
 			AND DATEPART(DAYOFYEAR, OrderDate) < DATEPART(DAYOFYEAR, Convert(datetime, '2006-03-01' )) -- Will become GETDATE
 			AND DATEPART(YEAR, OrderDate) < DATEPART(YEAR, convert(datetime, '2010-04-01'))
-		GROUP BY pro.Name
+		GROUP BY pro.ProductID
 
 	) as demcalc
-	group by [Name]
+	group by [ProductID]
 go
 
 exec CalculateDemand
 select * from DemandCalc
+
+select * from Production.Product
